6d7bb02703dbba7a150865692986163e768c4250
김재우 <63536145+Kjw19@users.noreply.github.com>
2023-05-29 11:49:07 +0900

    최초 커밋

---
+++ src/main/java/novel/social/novelcom/NovelcomApplication.java
@@ -0,0 +1,13 @@
+package novel.social.novelcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/novel/social/novelcom/NovelcomApplicationTests.java
@@ -0,0 +1,13 @@
+package novel.social.novelcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
